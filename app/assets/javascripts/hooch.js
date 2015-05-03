2ae36091cb8920db04a67493a6fb02b66fa3fa64
Eric Draut <edraut@gmail.com>
2015-05-03 12:01:41 -0600

    dynamically load anytime manager

--- app/assets/javascripts/hooch.js
+++ app/assets/javascripts/hooch.js
@@ -1,69 +1,3 @@
-/* Simple JavaScript Inheritance
- * By John Resig http://ejohn.org/
- * MIT Licensed.
- */
-// Inspired by base2 and Prototype
-(function(){
-  var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
-  // The base Class implementation (does nothing)
-  this.Class = function(){};
-
-  // Create a new Class that inherits from this class
-  Class.extend = function(prop) {
-    var _super = this.prototype;
-
-    // Instantiate a base class (but only create the instance,
-    // don't run the init constructor)
-    initializing = true;
-    var prototype = new this();
-    initializing = false;
-
-    // Copy the properties over onto the new prototype
-    for (var name in prop) {
-      // Check if we're overwriting an existing function
-      prototype[name] = typeof prop[name] == "function" &&
-        typeof _super[name] == "function" && fnTest.test(prop[name]) ?
-        (function(name, fn){
-          return function() {
-            var tmp = this._super;
-
-            // Add a new ._super() method that is the same method
-            // but on the super-class
-            this._super = _super[name];
-
-            // The method only need to be bound temporarily, so we
-            // remove it when we're done executing
-            var ret = fn.apply(this, arguments);
-            this._super = tmp;
-
-            return ret;
-          };
-        })(name, prop[name]) :
-        prop[name];
-    }
-
-    // The dummy class constructor
-    function Class() {
-      // All construction is actually done in the init method
-      if ( !initializing && this.init )
-        this.init.apply(this, arguments);
-    }
-
-    // Populate our constructed prototype object
-    Class.prototype = prototype;
-
-    // Enforce the constructor to be what we expect
-    Class.prototype.constructor = Class;
-
-    // And make this class extendable
-    Class.extend = arguments.callee;
-
-    return Class;
-  };
-})();
-/*End Simple Inheritance*/
-
-/*Begin Hooch.js*/
 var Toggler = Class.extend({
   init: function(jq_obj){
     this.jq_obj = jq_obj;
@@ -684,3 +618,16 @@ $(document).ready(function(){
     }
   });
 });
+if(typeof any_time_manager === "undefined"){
+    var any_time_script = document.createElement('script');
+    any_time_script.type = 'text/javascript';
+    any_time_script.src = "//rawgit.com/edraut/anytime_manager/master/anytime_manager.js";
+    document.body.appendChild(any_time_script);
+};
+any_time_manager.registerListWithClasses({
+  'expand-state' : 'Expandable', 'prevent-double-click' : 'PreventDoubleLinkClick'
+});
+any_time_manager.registerList(
+  'hover_overflow','hidey_button','submit-proxy','click_proxy','field-filler','revealer',
+  'checkbox-hidden-proxy','prevent-double-submit','prevent-double-link-click', 'tab-group',
+  'hover-reveal', 'emptier', 'remover');
