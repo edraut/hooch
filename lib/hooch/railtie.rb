23cd79ebf4ef2c1c85ae9bdb082f479b54fdd648
Eric Draut <edraut@gmail.com>
2016-02-02 16:21:06 -0700

    added stdout notification of helper initialization for debugging purposes

--- lib/hooch/railtie.rb
+++ lib/hooch/railtie.rb
@@ -2,6 +2,7 @@ require 'hooch/hooch_helper'
 module Hooch
   class Railtie < Rails::Railtie
     initializer "hooch.hooch_helper" do
+      puts "initializing hooch helpers"
       ActionView::Base.send :include, HoochHelper
     end
   end
