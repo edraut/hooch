691f7e95f2f343f11d10d24289482257182a9062
Eric Draut <edraut@gmail.com>
2017-10-25 11:55:40 -0600

    added polyfill for ES6 stuff

--- jasmine/karma.conf.js
+++ jasmine/karma.conf.js
@@ -16,6 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'test-main.js',
+      {pattern: 'node_modules/babel-polyfill/dist/polyfill.min.js'},
       {pattern: 'jquery.js'},
       {pattern: 'inheritance.js'},
       {pattern: 'jasmine-fixture.js'},
